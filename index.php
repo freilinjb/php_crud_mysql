7a3fd39fd2083dc01fad4d34951973d17aad086e
freilinjb <freilinjb@gmail.com>
2020-08-23 09:09:27 -0400

    diseñando formulario 22:05

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <?php
-    include("db.php");
+include("db.php");
 ?>
 
-<?php 
-    include("includes/header.php");
+<?php
+include("includes/header.php");
 ?>
 
-<h1>Hello Worlds</h1>
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+        <div class="card card-body">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="description" rows="2" placeholder="Task description" class="form-control"></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task">
+                </form>
+            </div>
+        </div>
 
+        <div class="col-md-8">
+           
+        </div>
+    </div>
+</div>
 
-<?php 
-    include("includes/footer.php");
+<?php
+include("includes/footer.php");
 ?>
-      
