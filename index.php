e63de7f9b7e0eb78ab77ccee2de0fb14cc844eee
freilinjb <freilinjb@gmail.com>
2020-08-23 19:48:35 -0400

    Add CDN and Icons

--- index.php
+++ index.php
@@ -14,13 +14,14 @@ include("includes/header.php");
             #Pregunta si existe un mensaje de la sesion para mostrarlo
             if (isset($_SESSION['message'])) { ?>
 
-                <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+                <div class="alert alert-<?= $_SESSION['message_type']; ?> alert-dismissible fade show" role="alert">
                     <strong><?= $_SESSION['message'] ?>!!</strong>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-            <?php  session_unset(); }
+            <?php session_unset();
+            }
             ?>
 
             <div class="card card-body">
@@ -47,26 +48,26 @@ include("includes/header.php");
                     </tr>
                 </thead>
                 <tbody>
-                    <?php 
-                        $query = "SELECT * FROM task";
-                        $result_tasks = mysqli_query($conn, $query);
+                    <?php
+                    $query = "SELECT * FROM task";
+                    $result_tasks = mysqli_query($conn, $query);
 
-                        while($row = mysqli_fetch_array($result_tasks)) { ?>
-                            <tr>
-                                <td><?php echo $row['title'] ?></td>
-                                <td><?php echo $row['description'] ?></td>
-                                <td><?php echo $row['create_at'] ?></td>
-                                <td>
-                                    <a href="edit.php?id=<?php echo $row['id']?>">
-                                        Edit
-                                    </a>
+                    while ($row = mysqli_fetch_array($result_tasks)) { ?>
+                        <tr>
+                            <td><?php echo $row['title'] ?></td>
+                            <td><?php echo $row['description'] ?></td>
+                            <td><?php echo $row['create_at'] ?></td>
+                            <td>
+                                <a href="edit.php?id=<?php echo $row['id'] ?>" class="btn btn-secondary">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
 
-                                    <a href="delete.php?id=<?php echo $row['id']?>">
-                                        Delete
-                                    </a>
-                                </td>
-                            </tr>
-                        <?php } ?>
+                                <a href="delete.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                 </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
