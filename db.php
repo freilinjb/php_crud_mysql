176f24dd392e9964bd5cb015169fd27375b2f76f
freilinjb <freilinjb@gmail.com>
2020-08-23 08:52:10 -0400

    realizando ajustes de la estructura

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'php_crud_mysql'
+);
+
+
+if(isset($conn)) {
+    echo "DB is connected";
+} else {
+    echo "DB not connected";
+    
+}
+?>
